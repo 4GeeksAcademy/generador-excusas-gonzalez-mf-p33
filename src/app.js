3ab68527fe8ec95ef2708f78704a199f3601c0c8
María Fernanda González <mafergonzalezw@gmail.com>
2024-07-15 23:31:28 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,35 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function aleatorio(lista) {
+    let numAleatorio = Math.floor(Math.random() * lista.length);
+    return lista[numAleatorio] + " ";
+  }
+
+  //who + action + what + when
+  let who = ["Taylor swift", " Ozuna ", "Ant-man", "Mario Bros"];
+  let action = ["vomito", "se comio", "reputo", "programo"];
+  let what = ["mi papelera", "el escalimetro", "un calvo"];
+  let when = [
+    "despues de sentarse en el carro",
+    "cuando estaba en el eras tour",
+    "mientras estaba en clase",
+    "antes de pisar clavos",
+    "mientras queria jugar valorant"
+  ];
+
+  let excuse =
+    aleatorio(who) + aleatorio(action) + aleatorio(what) + aleatorio(when);
+
+  let elementExcuse = window.document.querySelector("#excuse");
+
+  elementExcuse.innerHTML = excuse;
+
+  let excuseButton = window.document.querySelector("#generate-excuse");
+
+  excuseButton.addEventListener("click", () => {
+    let newExcuse =
+      aleatorio(who) + aleatorio(action) + aleatorio(what) + aleatorio(when);
+    elementExcuse.innerHTML = newExcuse;
+  });
 };
